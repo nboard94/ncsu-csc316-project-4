d75badc91d908b0c44cae2281d4d59d928368d91
NBoar <NBoar@Leonard>
2017-04-19 19:22:36 -0400

    Implemented the TextFileReader and SpellCheckerUI, imported test files.

--- SpellCheckManager/src/edu/ncsu/csc316/spell_checker/manager/SpellCheckerManager.java
+++ SpellCheckManager/src/edu/ncsu/csc316/spell_checker/manager/SpellCheckerManager.java
@@ -1,29 +1,46 @@
 package edu.ncsu.csc316.spell_checker.manager;
 
+import edu.ncsu.csc316.spell_checker.io.TextFileReader;
+import edu.ncsu.csc316.spell_checker.list.ArrayBasedList;
+
+/**
+ * @author Nicholas Board (ndboard) The engine of the SpellCheckManager program.
+ */
 public class SpellCheckerManager {
+
+	/** Contains all words in the dictionary. */
+	private ArrayBasedList<String> dictionary;
+
+	/** Contains all words in the input file. */
+	private ArrayBasedList<String> input;
+
+	/** TextFileReader to scan both the dictionary and input file. */
+	private TextFileReader fileReader;
 
 	/**
 	 * Constructs a new Spell Checker with the given dictionary
 	 * 
-	 * @param pathToDictionary the path to the dictionary
+	 * @param pathToDictionary
+	 *            the path to the dictionary
 	 */
-	public SpellCheckerManager(String pathToDictionary) 
-	{
-	    // Your code here
+	public SpellCheckerManager(String pathToDictionary) {
+		fileReader = new TextFileReader(pathToDictionary);
+		dictionary = fileReader.getWords();
 	}
-	
+
 	/**
-	 * Returns a string representation of the list of misspelled
-	 * words (in alphabetical order, case insensitive) in the input file.
-	 * The string representation should be in the format:
-	 * ArrayBasedList[a, b, c]
+	 * Returns a string representation of the list of misspelled words (in
+	 * alphabetical order, case insensitive) in the input file. The string
+	 * representation should be in the format: ArrayBasedList[a, b, c]
 	 * 
-	 * @param pathToFile the path to the file to be spell-checked
+	 * @param pathToFile
+	 *            the path to the file to be spell-checked
 	 * @return a string representation of the list of misspelled words
 	 */
-	public String spellCheck(String pathToFile)
-	{
+	public String spellCheck(String pathToFile) {
+		fileReader = new TextFileReader(pathToFile);
+		input = fileReader.getWords();
+
 		return pathToFile;
-	    // Your code here
 	}
 }
--- SpellCheckManager/src/edu/ncsu/csc316/spell_checker/ui/SpellCheckerUI.java
+++ SpellCheckManager/src/edu/ncsu/csc316/spell_checker/ui/SpellCheckerUI.java
@@ -6,72 +6,78 @@ import java.util.Scanner;
 import edu.ncsu.csc316.spell_checker.manager.SpellCheckerManager;
 
 /**
- * Controls the mechanisms that interfaces with the user.
- * Calls to the SpellCheckManager.
- * 
- * @author Nicholas Board (ndboard)
+ * @author Nicholas Board (ndboard) Controls the mechanisms that interfaces with
+ *         the user.
  */
 public class SpellCheckerUI {
 
-	/** String that represents filepath for dictionary file.  Set to test path by default. */
-	private String dictionaryFile = "input/dictionary.txt";
-	
-	/** String that represents filepath for input file.  Set to test path by default. */
-	private String inputFile = "input/input.txt";
-	
-	/** Scanner used to read user input. */
-	Scanner uiScan = new Scanner(System.in);
-	
-	/** The manager that handles the spell check functionality. */
-	SpellCheckerManager manager;
-	
 	/**
-	 * Where the SpellCheckManager begins execution.  Loop continuously
-	 * until the specified dictionary file exists.  Then loop continuously
-	 * allowing checks on different input files.
-	 * @param args Contains command-line arguments, if there are any.
+	 * String that represents filepath for dictionary file. Set to test path by
+	 * default.
 	 */
-	public void main(String[] args) {
-		
+	private static String dictionaryFile = "input/dictionary.txt";
+
+	/**
+	 * String that represents filepath for input file. Set to test path by
+	 * default.
+	 */
+	private static String inputFile = "input/input.txt";
+
+	/** Scanner used to read user input. */
+	private static Scanner uiScan = new Scanner(System.in);
+
+	/** The manager that handles the spell check functionality. */
+	private static SpellCheckerManager manager;
+
+	/** Used to check if a filepath is valid, or not. */
+	private static File testFile;
+
+	/**
+	 * Where the SpellCheckManager begins execution. Loop continuously until the
+	 * specified dictionary file exists. Then loop continuously allowing checks
+	 * on different input files.
+	 * 
+	 * @param args
+	 *            Contains command-line arguments, if there are any.
+	 */
+	public static void main(String[] args) {
+
 		System.out.println("Enter \"quit\" at anytime to exit the program.");
-		
-		while(true) {
-			
-			 System.out.print("Please specify dictionary file: ");
-			 dictionaryFile = uiScan.next();
-			
-			 if( dictionaryFile.toLowerCase().equals("quit"))
-				 System.exit(0);
-			 
-			 try {
-				 
-				 new File(dictionaryFile);
-				 
-				 manager = new SpellCheckerManager(dictionaryFile);
-				 while(true) {
-					 
-					 System.out.print("Please specify file to check: ");
-					 inputFile = uiScan.next();
-					 
-					 if( inputFile.toLowerCase().equals("quit"))
-						 System.exit(0);
-					 
-					 try {
-						 
-						 new File(inputFile);
-						 
-						 System.out.println(manager.spellCheck(inputFile));
-					 } catch (NullPointerException e) {
-						 
-						 System.out.println("Invalid input file, please enter a valid filepath.");
-					 }
-				 }
-				 
-			 }
-			 catch( NullPointerException e) {
-				 
-				 System.out.println("Invalid dictionary file, please enter a valid filepath.");
-			 }
+
+		while (true) {
+
+			System.out.print("Please specify dictionary file: ");
+			dictionaryFile = uiScan.next();
+
+			if (dictionaryFile.toLowerCase().equals("quit"))
+				System.exit(0);
+
+			testFile = new File(dictionaryFile);
+			if (testFile.exists()) {
+
+				manager = new SpellCheckerManager(dictionaryFile);
+				while (true) {
+
+					System.out.print("Please specify file to check: ");
+					inputFile = uiScan.next();
+
+					if (inputFile.toLowerCase().equals("quit"))
+						System.exit(0);
+
+					testFile = new File(inputFile);
+					if (testFile.exists()) {
+
+						System.out.println(manager.spellCheck(inputFile));
+					} else {
+
+						System.out.println("Invalid input file, please enter a valid filepath.");
+					}
+				}
+
+			} else {
+
+				System.out.println("Invalid dictionary file, please enter a valid filepath.");
+			}
 		}
 	}
 }
