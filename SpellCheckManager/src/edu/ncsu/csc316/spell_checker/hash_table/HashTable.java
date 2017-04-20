9059b9fa3f3e696e6d4a0b91b27bc16111ce6e89
NBoar <NBoar@Leonard>
2017-04-20 12:25:06 -0400

    Did I actually make the HashTable work?

--- SpellCheckManager/src/edu/ncsu/csc316/spell_checker/hash_table/HashTable.java
+++ SpellCheckManager/src/edu/ncsu/csc316/spell_checker/hash_table/HashTable.java
@@ -1,5 +1,6 @@
 package edu.ncsu.csc316.spell_checker.hash_table;
 
+import edu.ncsu.csc316.spell_checker.counter.Counter;
 import edu.ncsu.csc316.spell_checker.list.ArrayBasedList;
 import edu.ncsu.csc316.spell_checker.word.Word;
 
@@ -10,89 +11,93 @@ import edu.ncsu.csc316.spell_checker.word.Word;
 public class HashTable {
 
 	/** Size to initialize the HashTable's array to. */
-	private final static int TABLE_SIZE = 128;
+	private final static int TABLE_SIZE = 1000000;
 
 	/** The HashTable array. */
 	private Word[] table;
+	
+	/** The HashTable size. */
+	private int size;
+	
+	/** The Counter to keep track of probes. */
+	private Counter counter = new Counter();
 
 	/**
-	 * Constructs a HashTable. Initialized all entries to null.
-	 * 
-	 * @param dictionary
-	 *            The list of words in the dictionary, add to the HashTable.
-	 * @param input
-	 *            The list of words in the input file, convert to array of Word.
+	 * Constructs a HashTable.
 	 */
-	public HashTable(ArrayBasedList<String> dictionary) {
+	public HashTable() {
 
 		table = new Word[TABLE_SIZE];
-		for (int i = 0; i < TABLE_SIZE; i++) {
-			table[i] = null;
-		}
-
-		for (int i = 0; i < dictionary.size(); i++ ) {
-			
+		size = 0;
+	}
+	
+	public void buildHashTable(ArrayBasedList<String> dictionary) {
+		
+		for( int i= 0; i < dictionary.size(); i++ )
 			this.insert(dictionary.lookUp(i));
-		}
 	}
 
-	/**
-	 * Adds a word to the hash table.
-	 * 
-	 * @param value
-	 *            The value of the word to add.
-	 */
+	//TODO Implement this
 	public void insert(String value) {
 
 		Word newWord = new Word(value);
 		int hash = this.getHashFromKey(newWord.hashCode());
-
-		if (table[hash] == null)
-
+		
+		if(table[hash] == null)
 			table[hash] = newWord;
 		else {
-
+			
 			Word entry = table[hash];
-
-			while (entry.getNext() != null && entry.hashCode() != newWord.hashCode())
+			
+			while(entry.getNext() != null)
 				entry = entry.getNext();
+			
+			entry.setNext(newWord);
+		}
+		
+		this.size++;
+	}
 
-			if (entry.hashCode() == newWord.hashCode())
-				entry.setWord(value);
+	//TODO Implement this
+	public String lookUp(String word) {
+		
+		int hash = this.getHashFromKey(word.hashCode());
+		
+		if(table[hash] == null)
+			return null;
+		else {
+			
+			Word entry = table[hash];
+			
+			while(entry != null && entry.getWord().hashCode() != word.hashCode())
+				entry = entry.getNext();
+			
+			if(entry.getNext() == null && entry.getWord().hashCode() != word.hashCode())
+				return null;
 			else
-				entry.setNext(newWord);
+				return entry.getWord();
+			
 		}
 	}
 
 	/**
-	 * Looks up a Word in the hash table.
-	 * 
-	 * @param key
-	 *            the hashcode of the Word to lookup.
-	 * @return the found word.
+	 * Tells the size of the HashTable.
+	 * @return The size of the table.
 	 */
-	public String lookUp(int key) {
-
-		int hash = this.getHashFromKey(key);
-
-		if (table[hash] == null)
-
-			return null;
-		else {
-
-			Word entry = table[hash];
-
-			while (entry != null && entry.hashCode() != hash)
-				entry = entry.getNext();
-
-			if (entry == null)
-				return null;
-			else
-				return entry.getWord();
-		}
-
+	public int size() {
+		
+		return this.size;
 	}
-
+	
+	/**
+	 * Gets the counter used in this HashTable.
+	 * @return The HashTable's counter.
+	 */
+	public Counter getCount() {
+		
+		return this.counter;
+	}
+	
 	/**
 	 * The Hashing function used in the table. Currently uses division hashing.
 	 * TODO change to MAD or Golden Ratio if needed.
@@ -103,6 +108,11 @@ public class HashTable {
 	 */
 	private int getHashFromKey(int key) {
 
-		return (key % TABLE_SIZE);
+		int hash = ( key % TABLE_SIZE);
+		
+		if( hash < 0)
+			hash *= -1;
+			
+		return hash;
 	}
 }
--- SpellCheckManager/src/edu/ncsu/csc316/spell_checker/word/Word.java
+++ SpellCheckManager/src/edu/ncsu/csc316/spell_checker/word/Word.java
@@ -61,9 +61,7 @@ public class Word {
 	 */
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((word == null) ? 0 : word.hashCode());
-		return result;
+
+		return this.word.hashCode();
 	}
 }
