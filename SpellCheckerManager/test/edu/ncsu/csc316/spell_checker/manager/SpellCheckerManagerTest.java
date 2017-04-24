a8f6f044dbea075e5de46d4cac7724a5566491e2
NBoar <NBoar@Leonard>
2017-04-23 23:17:00 -0400

    Implemented testing for the SpellCheckerManager.

---
+++ SpellCheckerManager/test/edu/ncsu/csc316/spell_checker/manager/SpellCheckerManagerTest.java
@@ -0,0 +1,29 @@
+package edu.ncsu.csc316.spell_checker.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests the SpellCheckerManager.
+ * 
+ * @author Nicholas Board (ndboard)
+ */
+public class SpellCheckerManagerTest {
+
+	
+	private String pathToD = "input/dictionary.txt";
+	private String pathToI = "input/input.txt";
+	private String misspelled = "and, are, However, most, of, those";
+	
+	/**
+	 * Test the SpellCheckerManager.
+	 */
+	@Test
+	public void testSpellCheckManager() {
+		
+		SpellCheckerManager manager = new SpellCheckerManager(pathToD);
+		
+		assertEquals(misspelled, manager.spellCheck(pathToI));
+	}
+}
